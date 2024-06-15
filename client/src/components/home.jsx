0e564a64078befd185674cf712a9a5d8d6233e51
praneetha-8 <bolneti_praneetha@srmap.edu.in>
2024-06-15 21:39:04 +0530

    Home page

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import ashImage from '../../public/ash.png'; // Adjust the path as necessary
-import pikachuImage from '../../public/Pokemon-3.png'; // Adjust the path as necessary
-import squirtleImage from '../../public/Pokemon-2.png'; // Adjust the path as necessary
-import aptos from '../../public/aptoslogo.png'; // Adjust the path as necessary
 
 export default function Home() {
   const handleDisconnect = async () => {
@@ -15,21 +11,21 @@ export default function Home() {
       <div className="home-bg-image relative min-h-screen flex flex-col justify-between">
         <header className="w-full text-white flex justify-center pt-0 mt-0 relative">
           <div className="bg-black bg-opacity-60 px-12 py-5 flex justify-between items-center rounded-lg w-full max-w-screen-lg mx-auto">
-            <nav className="flex space-x-12 text-lg" style={{ fontFamily: "'Comic Sans MS', 'Comic Sans', cursive" }}>
+            <nav className="flex space-x-6 text-lg" style={{ fontFamily: "'Press Start 2P', cursive" }}>
               <a href="/deck" className="hover:underline">Cards Deck</a>
-              <span>/</span>
+              <img src="/pokeball.png" alt="Pokeball" className="h-8 my-auto" /> {/* Increase size */}
               <a href="/marketplace" className="hover:underline">Market Place</a>
-              <span>/</span>
+              <img src="/pokeball.png" alt="Pokeball" className="h-8 my-auto" /> {/* Increase size */}
               <a href="/arena" className="hover:underline">Battle Arena</a>
-              <span>/</span>
+              <img src="/pokeball.png" alt="Pokeball" className="h-8 my-auto" /> {/* Increase size */}
               <a href="/league" className="hover:underline">League Events</a>
             </nav>
-            <div className="flex items-center bg-black bg-opacity-60 px-4 py-2 rounded-lg">
+            <div className="flex items-center bg-black bg-opacity-60 px-4 py-2 rounded-lg ml-6">
               <span className="text-white mr-2">2.000</span>
-              <img src={aptos} alt="Logo" className="h-10" />
+              <img src="/aptoslogo.png" alt="Logo" className="h-10" />
               <button 
                 onClick={handleDisconnect} 
-                className="ml-4 px-4 py-2 bg-red-500 hover:bg-red-700 text-white rounded-lg"
+                className="ml-4 px-4 py-2 bg-red-500 hover:bg-red-700 text-white rounded-lg transition-colors duration-300"
               >
                 Disconnect
               </button>
@@ -37,12 +33,12 @@ export default function Home() {
           </div>
         </header>
         <div className="flex flex-grow justify-center items-center relative">
-          <img src={ashImage} alt="Ash with Pokeball" className="h-auto max-w-xs transform translate-y-10" /> {/* Move Ash further down */}
+          <img src="/ash.png" alt="Ash with Pokeball" className="h-auto max-w-xs transform translate-y-10" />
           <div className="absolute left-0 top-full transform -translate-y-[135%] translate-x-[370%]">
-            <img src={pikachuImage} alt="Pikachu" className="h-auto" style={{ maxHeight: '200px' }} />
+            <img src="/Pokemon-3.png" alt="Pikachu" className="h-auto" style={{ maxHeight: '200px' }} />
           </div>
           <div className="absolute left-0 top-full transform -translate-y-[142%] translate-x-[220%]">
-            <img src={squirtleImage} alt="Squirtle" className="h-auto" style={{ maxHeight: '200px' }} />
+            <img src="/Pokemon-2.png" alt="Squirtle" className="h-auto" style={{ maxHeight: '200px' }} />
           </div>
         </div>
       </div>
